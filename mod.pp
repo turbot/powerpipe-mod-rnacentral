8718a0ddb4b09e5d74819ed9600529316255fa68
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-05 21:24:11 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -1,6 +1,7 @@
 mod "rnacentral" {
   title         = "RNAcentral"
   description   = "RNAcentral is a collection representing all ncRNA types from a broad range of organisms data using PostgreSQL and Powerpipe."
+  color         = "#3E8EB8"
   documentation = file("./README.md")
   categories    = ["dashboard", "postgres"]
 }
