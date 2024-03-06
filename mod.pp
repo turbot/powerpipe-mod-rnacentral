15d902b4cdab396d11f7893e2451a61b8eb4a2c7
misraved <ved@turbot.com>
2024-03-06 21:54:53 +0530

    Udpate the social graphics and icon name in the mod.pp file

--- mod.pp
+++ mod.pp
@@ -3,12 +3,12 @@ mod "rnacentral" {
   description   = "Visualize RNAcentral chemical components, database insights, and sequence behaviors, and more using Powerpipe and PostgreSQL."
   color         = "#3E8EB8"
   documentation = file("./docs/index.md")
-  icon          = "/images/mods/turbot/rnacentral.svg"
+  icon          = "/images/mods/turbot/rnacentral-insights.svg"
   categories    = ["dashboard", "postgres"]
 
   opengraph {
     title       = "Powerpipe Mod for RNAcentral"
     description = "Visualize RNAcentral chemical components, database insights, and sequence behaviors, and more using Powerpipe and PostgreSQL."
-    image       = "/images/mods/turbot/rnacentral-social-graphic.png"
+    image       = "/images/mods/turbot/rnacentral-insights-social-graphic.png"
   }
 }
