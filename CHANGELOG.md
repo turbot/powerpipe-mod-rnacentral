542605976b8d006ed289af9e436c8bfee66b65b3
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2024-03-06 19:07:13 +0530

    Add Powerpipe Mod Dashboard page forRnacentral (#1)
    Co-authored-by: sourav chakraborty <sourav@turbot.com>
    Co-authored-by: souravTurbot <78197905+bigdatasourav@users.noreply.github.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## v1.0.0 [2024-03-06]
+
+_What's new?_
+
+- New dashboard:
+  - [RNAcentral Dashboard](https://hub.powerpipe.io/mods/turbot/powerpipe-mod-rnacentral/dashboard.rnacentral)
---
+++ mod.pp
@@ -0,0 +1,13 @@
+mod "rnacentral" {
+  title         = "RNAcentral"
+  description   = "Visualize RNAcentral chemical components, database insights, and sequence behaviors, and more using Powerpipe and PostgreSQL."
+  color         = "#3E8EB8"
+  documentation = file("./README.md")
+  categories    = ["dashboard", "postgres"]
+
+  opengraph {
+    title       = "Powerpipe Mod for RNAcentral"
+    description = "Visualize RNAcentral chemical components, database insights, and sequence behaviors, and more using Powerpipe and PostgreSQL."
+    image       = "/images/mods/turbot/rnacentral-social-graphic.png"
+  }
+}
