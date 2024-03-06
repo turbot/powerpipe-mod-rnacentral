e81269f5612ec1fff8aa99585f231350aad6a244
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-06 14:33:49 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -8,6 +8,6 @@ mod "rnacentral" {
 
 opengraph {
   title       = "Powerpipe Mod for RNAcentral"
-  description = "Visualize COVID-19 vaccinations, healthcare capacity, regional trends, and more using Powerpipe and PostgreSQL."
+  description = "Visualize RNAcentral chemical components, database insights, and sequence behaviors, and more using Powerpipe and PostgreSQL."
   image       = "/images/mods/turbot/rnacentral-social-graphic.png"
 }
